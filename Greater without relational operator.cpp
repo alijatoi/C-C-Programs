e7677c3a505fdb358ff1b4b14a749e31c18fa43e
Ali Hassan <alihasanjatoi@gmail.com>
2020-10-10 16:10:01 +0500

    Add files via upload

---
+++ Greater without relational operator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter Two Numbers ";
+	cin>>a>>b;
+	if(a/b)
+	{
+		cout<<"Number "<<a<<" is Greater ";
+	}
+	else
+	cout<<"Number "<<b<<" is Greater";
+}
